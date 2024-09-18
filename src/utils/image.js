573b4228d5afb97cdd71477f7f5f35f518ed5e4c
applefi87 <55140921+applefi87@users.noreply.github.com>
2024-09-18 08:07:34 +0800

    fix: image size wrong

--- src/utils/image.js
+++ src/utils/image.js
@@ -3,6 +3,13 @@ import ValidationObjectError from '../infrastructure/errors/ValidationObjectErro
 import UnknownError from '../infrastructure/errors/UnknownError.js'
 import imageConfigs from '../infrastructure/configs/imageConfigs.js'
 
+const previewQuality = imageConfigs.previewImage.quality
+const previewWidth = imageConfigs.previewImage.width
+const previewHeight = imageConfigs.previewImage.height
+const normalQuality = imageConfigs.normalImage.quality
+const normalWidth = imageConfigs.normalImage.width
+const normalHeight = imageConfigs.normalImage.height
+
 export async function processImage(buffer, format, isPreview = false) {
   try {
     if (['tiff', 'svg+xml'].includes(format)) {
@@ -14,9 +21,9 @@ export async function processImage(buffer, format, isPreview = false) {
       throw new ValidationObjectError("InvalidImageFormat", { format: "jpg,png,webp" });
     }
 
-    const quality = isPreview ? imageConfigs.normalImage.quality : imageConfigs.previewImage.quality;
-    const width = isPreview ? imageConfigs.normalImage.width : imageConfigs.previewImage.width;
-    const height = isPreview ? imageConfigs.normalImage.height : imageConfigs.previewImage.height;
+    const quality = isPreview ? previewQuality : normalQuality;
+    const width = isPreview ? previewWidth : normalWidth;
+    const height = isPreview ? previewHeight : normalHeight;
 
     return getSharpInstance(buffer, 'webp', quality, width, height).toBuffer();
   } catch (error) {
@@ -24,7 +31,7 @@ export async function processImage(buffer, format, isPreview = false) {
   }
 }
 
-export function getSharpInstance(buffer = null, format = 'webp', quality = imageConfigs.previewImage.quality, width = imageConfigs.previewImage.width, height = previewImage.height) {
+export function getSharpInstance(buffer = null, format = 'webp', quality = previewQuality, width = previewWidth, height = previewHeight) {
   if (buffer === null) {
     return sharp().toFormat(format, { quality })
       .resize({ width, height, fit: 'inside', withoutEnlargement: true });
